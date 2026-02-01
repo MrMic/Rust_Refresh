75abe48a892287a187c36f31991a75c188188914
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-02-01 10:29:35 +0100

    feat: println!()

--- user_input/src/main.rs
+++ user_input/src/main.rs
@@ -1,24 +1,8 @@
-//! Crate about reading line from IO
-use std::io;
-
-/// Main function
-/// ```
-///fn main()
-/// ```
-/// Reads a line from stdin and echoes it back to the user.
 fn main() {
-    let mut input = String::new();
-
-    // Prompt the user for input
-    // fn main()
-    println!("Say something:");
-
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => {
-            println!("You said: {}", input.trim());
-        }
-        Err(e) => {
-            eprintln!("Something went wrong: {}", e);
-        }
-    }
+    println!("Hello, my name is: {}", "Mic");
+    println!("{0} has {1} dog.", "Toto", 2 + 1);
+    println!("{name} has a {pet}.", name = "Alice", pet = "cat");
+    println!("binary: {:b} - Hex: {:x} - Octal: {:o}", 5, 5, 5);
+    println!("Array: {:?}", [1, 2, 3]);
+    println!("Right aligned: {:>5}", 10);
 }
