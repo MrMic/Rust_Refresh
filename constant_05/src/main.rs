7818a7512aa475c9d3a942cb6315c96184e1baaf
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-02-07 11:30:20 +0100

    feat(constants): add constant_05 / CHAP 03

---
+++ constant_05/src/main.rs
@@ -0,0 +1,9 @@
+#[allow(unused_variables)]
+const URL: &str = "google.com";
+
+fn main() {
+    // INFO: CONSTANTS | UPPERCASE | DATA TYPES MANDATORY
+    // INFO: Global scope, accessible from anywhere or local scope, accessible only within the
+    // block it is defined in
+    println!("🪚 URL: {}", URL);
+}
