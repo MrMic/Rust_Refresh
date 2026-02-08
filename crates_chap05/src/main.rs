6bdd48329e8c4f60cdf5c12c5588a2f56ef0cb8f
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-02-08 11:11:58 +0100

    ✨ feat(04/crates): Add more random generation capabilities to main.rs, including bounded int and float generation, and random string generation using Alphanumeric distribution.

--- crates_chap05/src/main.rs
+++ crates_chap05/src/main.rs
@@ -1,6 +1,6 @@
 // use crate::archive::arch::arch_file;
 use crate::archive::arch::arch_file as arc;
-use rand::Rng;
+use rand::{Rng, distr::Alphanumeric};
 
 mod archive;
 
@@ -11,4 +11,14 @@ fn main() {
     let mut rng = rand::rng();
     let a: i32 = rng.random();
     println!("Random number: {}", a);
+
+    println!("Bounded int: {}", rng.random_range(1..=10));
+    println!("Bounded float: {}", rng.random_range(0.0..=100.0));
+
+    let rand_string: String = rng
+        .sample_iter(&Alphanumeric)
+        .take(10) // INFO: Only 10 chars
+        .map(char::from)
+        .collect();
+    println!("Random string: {}", rand_string);
 }
