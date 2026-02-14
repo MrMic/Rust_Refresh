617599d4a145ec2b08764e4598c8718b86205335
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-02-14 13:06:42 +0100

    feat(05/structure): Structure + Impl

--- data_types_05/src/main.rs
+++ data_types_05/src/main.rs
@@ -1,17 +1,45 @@
 #![allow(dead_code)]
+
 #[allow(unused_variables)]
 fn main() {
-    // INFO: Tuples: Collection of values of varous types
-    // WARN: Static, cannot be resized - Elements can be updated
-    let mut person: (&str, i64, bool) = ("John", 27, true);
-    println!("🪚 person => {:?}", person);
-    println!("🪚 person.0 => {:?}", person.0);
-    println!("------------------------");
+    // NOTE: STRUCTURE
+    let emp = Employe {
+        name: String::from("John"),
+        company: String::from("Google"),
+        age: 35,
+    };
 
-    person.2 = false;
-    println!("🪚 person => {:?}", person);
-    println!("------------------------");
+    println!("emp => {:?}", emp);
+    println!("emp.name => {}", emp.name);
+    println!("emp.company => {}", emp.company);
+    println!("emp.age => {}", emp.age);
 
-    let (name, age, employed) = person;
-    println!("🪚 name: {}, age: {}, employed: {}", name, age, employed); // WARN: Destructuring
+    println!("-------------------------");
+
+    println!("  emp => {}", emp.fn_details());
+    // println!("emp => {}", Employe::fn_details(&emp));
+
+    println!("{}", Employe::static_fn_details());
+}
+
+#[derive(Debug)]
+struct Employe {
+    name: String,
+    company: String,
+    age: u32,
+}
+
+// WARN: Add method to the struct
+impl Employe {
+    fn fn_details(&self) -> String {
+        format!(
+            "name: {}, company: {}, age: {}",
+            &self.name, &self.company, &self.age
+        )
+    }
+
+    // WARN: Static method ( without &self)
+    fn static_fn_details() -> String {
+        String::from("👨‍🍼  Details of an employe")
+    }
 }
