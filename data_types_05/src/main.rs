7de007cd969a696efcd0b94de4313c908a176766
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-02-15 13:09:50 +0100

    feat(05/enum): Enum

--- data_types_05/src/main.rs
+++ data_types_05/src/main.rs
@@ -1,45 +1,30 @@
 #![allow(dead_code)]
+use crate::Colors::Blue;
+use crate::Person::*;
+
+#[derive(Debug)]
+enum Colors {
+    Red,
+    Green,
+    Blue,
+}
+
+// INFO: Add Data Type to enum's members
+#[derive(Debug)]
+enum Person {
+    Name(String),
+    Surname(String),
+    Age(u32),
+}
 
 #[allow(unused_variables)]
 fn main() {
-    // NOTE: STRUCTURE
-    let emp = Employe {
-        name: String::from("John"),
-        company: String::from("Google"),
-        age: 35,
-    };
+    let my_color = Colors::Red;
+    println!("My color is: {my_color:?}");
 
-    println!("emp => {:?}", emp);
-    println!("emp.name => {}", emp.name);
-    println!("emp.company => {}", emp.company);
-    println!("emp.age => {}", emp.age);
+    let my_color = Blue;
+    println!("------------------------");
 
-    println!("-------------------------");
-
-    println!("  emp => {}", emp.fn_details());
-    // println!("emp => {}", Employe::fn_details(&emp));
-
-    println!("{}", Employe::static_fn_details());
-}
-
-#[derive(Debug)]
-struct Employe {
-    name: String,
-    company: String,
-    age: u32,
-}
-
-// WARN: Add method to the struct
-impl Employe {
-    fn fn_details(&self) -> String {
-        format!(
-            "name: {}, company: {}, age: {}",
-            &self.name, &self.company, &self.age
-        )
-    }
-
-    // WARN: Static method ( without &self)
-    fn static_fn_details() -> String {
-        String::from("👨‍🍼  Details of an employe")
-    }
+    let person = Name("John".to_string());
+    println!("Person is: {person:?}");
 }
