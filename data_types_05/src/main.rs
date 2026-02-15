5961ffec2e2f6fa74d7b287bcd7f7873f922d50c
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-02-15 13:20:06 +0100

    feat(05/generics): Generics

--- data_types_05/src/main.rs
+++ data_types_05/src/main.rs
@@ -1,30 +1,27 @@
 #![allow(dead_code)]
-use crate::Colors::Blue;
-use crate::Person::*;
-
-#[derive(Debug)]
-enum Colors {
-    Red,
-    Green,
-    Blue,
-}
-
-// INFO: Add Data Type to enum's members
-#[derive(Debug)]
-enum Person {
-    Name(String),
-    Surname(String),
-    Age(u32),
-}
+use Colors::*;
 
 #[allow(unused_variables)]
 fn main() {
-    let my_color = Colors::Red;
-    println!("My color is: {my_color:?}");
+    let p1: Point<i32> = Point { x: 5, y: 10 };
+    let p2: Point<f32> = Point { x: 1.0, y: 4.0 };
+    println!("p1: {:?}", p1);
+    println!("p2: {:?}", p2);
 
-    let my_color = Blue;
-    println!("------------------------");
+    let c1 = Red("#ff0000");
+    println!("c1: {:?}", c1);
+}
 
-    let person = Name("John".to_string());
-    println!("Person is: {person:?}");
+// ______________________________________________________________________
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+#[derive(Debug)]
+enum Colors<T> {
+    Red(T),
+    Blue(T),
+    Green(T),
 }
