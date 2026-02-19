3be8a9e83131b4800ca2b870b30b279acb9e5270
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-02-19 16:20:00 +0100

    feat(07/functions): Closures & Generics functions

---
+++ functions_07/src/main.rs
@@ -0,0 +1,19 @@
+#![allow(unused_variables)]
+#![allow(unused_assignments)]
+
+use std::fmt::Display;
+
+fn main() {
+    // INFO: Closures are anonymous functions that can capture variables from their surrounding
+    // scope. They are often used for short-lived operations or when you want to pass a function as
+    // an argument to another function.
+    let a = |a: i32| -> i32 { a + 1 };
+    println!("a(5) = {}", a(5));
+
+    // INFO: Generic functions are functions that can operate on different types without needing to
+    fn prn<T: Display>(x: T) {
+        println!("{}", x)
+    }
+    prn("Hello, world!");
+    prn(42);
+}
