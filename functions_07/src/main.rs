a3056596b80386838233edf3dbf2ca3fd1fdeb17
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-02-20 16:31:35 +0100

    feat(07/functions): High Orfre Funciotns (HOFs)

--- functions_07/src/main.rs
+++ functions_07/src/main.rs
@@ -1,19 +1,16 @@
 #![allow(unused_variables)]
 #![allow(unused_assignments)]
 
-use std::fmt::Display;
-
 fn main() {
-    // INFO: Closures are anonymous functions that can capture variables from their surrounding
-    // scope. They are often used for short-lived operations or when you want to pass a function as
-    // an argument to another function.
-    let a = |a: i32| -> i32 { a + 1 };
-    println!("a(5) = {}", a(5));
+    // INFO: High Order Functions
+    let square = |a: i32| a * a;
+    apply(square, 5);
+}
 
-    // INFO: Generic functions are functions that can operate on different types without needing to
-    fn prn<T: Display + ?Sized>(x: &T) {
-        println!("{}", x)
-    }
-    prn("Hello, world!");
-    prn(&42);
+// ______________________________________________________________________
+fn apply<F>(f: F, a: i32)
+where
+    F: Fn(i32) -> i32,
+{
+    println!("Result: {}", f(a));
 }
