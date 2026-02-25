43b78e4a7e768d03fb0d7304e9acb7b341b061e2
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-02-25 15:04:27 +0100

    feat: add Summable trait and implement for Vec<i32>
    - Operator overloading
    Replaces previous Animal trait example with a new Summable trait.
    Implements Summable for Vec<i32> to provide a custom sum method.
    Demonstrates trait usage in main by summing a vector of integers.
    This showcases Rust's trait system and extension of existing types.

--- trait_08/src/main.rs
+++ trait_08/src/main.rs
@@ -1,26 +1,31 @@
 #![allow(unused_variables)]
 #![allow(unused_assignments)]
 
-//INFO: STRUCTS _________________________________________________________
+use std::ops::Add;
 
-//INFO: TRAITS & IMPL ___________________________________________________
-trait Summable<T> {
-    fn sum(&self) -> T;
+//INFO: STRUCTS _________________________________________________________
+#[derive(Debug)]
+struct Point {
+    x: f64,
+    y: f64,
 }
 
-impl Summable<i32> for Vec<i32> {
-    fn sum(&self) -> i32 {
-        let mut sum = 0;
-        for &num in self {
-            sum += num;
+impl Add for Point {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
         }
-        sum
     }
 }
 
+//INFO: TRAITS & IMPL ___________________________________________________
+
 // ______________________________________________________________________
 fn main() {
-    let a = vec![1, 2, 3, 4, 5];
-    let result = a.sum();
-    println!("The sum of the vector is: {}", result);
+    let p1 = Point { x: 1.0, y: 2.0 };
+    let p2 = Point { x: 3.0, y: 4.0 };
+    let p3 = p1 + p2;
+    println!("{:?}", p3);
 }
