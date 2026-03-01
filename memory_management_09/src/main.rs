e77a4269f0ea664afc74147fbe8d3c6be4eb515a
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-03-01 11:23:04 +0100

    feat(memory_management_09): Borrowing (mutable vs immutable) + reference

--- memory_management_09/src/main.rs
+++ memory_management_09/src/main.rs
@@ -1,22 +1,23 @@
 #![allow(unused_variables)]
 #![allow(unused_assignments)]
 
+// INFO: Borrowing and References
 fn main() {
-    let i = 32;
-    let j = i;
-    println!("i: {}, j: {}", i, j);
+    let mut a = 6;
+    {
+        let b = &mut a;
+        println!("b: {}", *b);
+        *b += 2;
+    }
+    println!("a: {}", a);
 
+    //---------------------------
+    println!("---------------------------");
     let v = vec![1, 2, 3];
-    // let w = v;
-    // println!("w: {:?}", w);
-    // WARN: The following line will cause a compile-time error because v has been moved to w
-    // println!("v: {:?}", v);
+    for i in &v {
+        println!("i: {}", i);
 
-    let foo = |v: Vec<i32>| -> Vec<i32> {
-        println!("vector used in foo: {:?}", v);
-        v
-    };
-
-    let v = foo(v);
-    println!("v after foo: {:?}", v);
+        // WARN: cannot borrow `v` as mutable because it is also borrowed as immutable
+        // v.push(4);
+    }
 }
