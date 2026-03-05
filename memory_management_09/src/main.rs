7ffe51f71b4e662993b047c1632a75ee28a3e948
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-03-05 14:49:07 +0100

    feat(memory_management_09): add examples for lifetimes and struct references
       - Implement Person and Dog structs to demonstrate lifetime annotations in
       structs.
       - Add examples for lifetime elision in method signatures.
       - Demonstrate static lifetimes and scope-based borrow checking.

--- memory_management_09/src/main.rs
+++ memory_management_09/src/main.rs
@@ -1,23 +1,53 @@
 #![allow(unused_variables)]
 #![allow(unused_assignments)]
+#![allow(dead_code)]
 
-// INFO: Borrowing and References
+#[derive(Debug)]
+struct Person {
+    name: String,
+}
+
+impl Person {
+    // INFO: Lifetime elision!
+    // fn get_name<'a>(&'a self) -> &'a String {
+    fn get_name(&self) -> &String {
+        &self.name
+    }
+}
+
+// INFO: struct Object<'lifetime> {
+// INFO:   field: &'lifetime Type,
+// INFO:  ... }
+#[derive(Debug)]
+struct Dog<'a> {
+    name: String,
+    owner: &'a Person,
+}
+
+// INFO: Lifetime
 fn main() {
-    let mut a = 6;
+    println!("{}", get_str());
+
+    let p1 = Person {
+        name: String::from("Alice"),
+    };
+    let d1 = Dog {
+        name: String::from("Buddy"),
+        owner: &p1,
+    };
+    println!("{:?}", d1);
+
+    let a: &String;
     {
-        let b = &mut a;
-        println!("b: {}", *b);
-        *b += 2;
+        let p2 = Person {
+            name: String::from("Bob"),
+        };
+        // a = p2.get_name();
+        a = p1.get_name();
     }
-    println!("a: {}", a);
+    println!("{}", a);
+}
 
-    //---------------------------
-    println!("---------------------------");
-    let v = vec![1, 2, 3];
-    for i in &v {
-        println!("i: {}", i);
-
-        // WARN: cannot borrow `v` as mutable because it is also borrowed as immutable
-        // v.push(4);
-    }
+fn get_str() -> &'static str {
+    "Hello!"
 }
