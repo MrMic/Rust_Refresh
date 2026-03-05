848a4e3248bd2d825e412aef6e58189ec71f53ee
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-03-05 15:40:30 +0100

    feat(memory_management_09): introduce reference counting with Rc<T>
    - Replace lifetime and struct reference examples with Rc<T> smart pointer demonstration.
    - Implement Car struct using Rc<String> to show shared ownership.
    - Add code to monitor reference counts using Rc::strong_count across different scopes.

--- memory_management_09/src/main.rs
+++ memory_management_09/src/main.rs
@@ -2,52 +2,30 @@
 #![allow(unused_assignments)]
 #![allow(dead_code)]
 
-#[derive(Debug)]
-struct Person {
-    name: String,
+use std::rc::Rc;
+
+struct Car {
+    brand: Rc<String>,
 }
 
-impl Person {
-    // INFO: Lifetime elision!
-    // fn get_name<'a>(&'a self) -> &'a String {
-    fn get_name(&self) -> &String {
-        &self.name
+impl Car {
+    fn new(brand: Rc<String>) -> Self {
+        Car { brand }
+    }
+    fn drive(&self) {
+        println!("Driving a {} car!", self.brand);
     }
 }
 
-// INFO: struct Object<'lifetime> {
-// INFO:   field: &'lifetime Type,
-// INFO:  ... }
-#[derive(Debug)]
-struct Dog<'a> {
-    name: String,
-    owner: &'a Person,
-}
-
-// INFO: Lifetime
+// INFO: Reference Counter
+// - Rc<T> (Reference Counted Smart Pointer)
 fn main() {
-    println!("{}", get_str());
-
-    let p1 = Person {
-        name: String::from("Alice"),
-    };
-    let d1 = Dog {
-        name: String::from("Buddy"),
-        owner: &p1,
-    };
-    println!("{:?}", d1);
-
-    let a: &String;
+    let brand = Rc::new(String::from("Toyota"));
+    println!("Reference count: {}", Rc::strong_count(&brand));
     {
-        let p2 = Person {
-            name: String::from("Bob"),
-        };
-        // a = p2.get_name();
-        a = p1.get_name();
+        let car2 = Car::new(brand.clone());
+        println!("Reference count: {}", Rc::strong_count(&brand));
+        car2.drive();
     }
-    println!("{}", a);
-}
-
-fn get_str() -> &'static str {
-    "Hello!"
+    println!("Reference count: {}", Rc::strong_count(&brand));
 }
