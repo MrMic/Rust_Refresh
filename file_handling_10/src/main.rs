ba8b533a2b48e1ff9211221da7cbd41604fab1bb
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-03-10 13:15:47 +0100

    feat(file_handling_10): implement error propagation with the '?' operator
    - Add `read_username_from_file` function to demonstrate concise error handling.
    - Use the `?` operator to propagate errors from `File::open` and `read_to_string`.
    - Add a sample `username.txt.OK` file for testing the file reading logic.

--- file_handling_10/src/main.rs
+++ file_handling_10/src/main.rs
@@ -1,9 +1,29 @@
 #![allow(unused_variables)]
 #![allow(unused_assignments)]
 
-use std::fs::File;
+use std::{fs::File, io::Error, io::Read};
+
+// fn read_username_from_file() -> Result<String, Error> {
+//     let f = File::open("file_handling_10/src/username.txt");
+//     let mut f = match f {
+//         Ok(file) => file,
+//         Err(e) => return Err(e),
+//     };
+//
+//     let mut s = String::new();
+//     match f.read_to_string(&mut s) {
+//         Ok(_) => Ok(s),
+//         Err(e) => Err(e),
+//     }
+// }
+
+fn read_username_from_file() -> Result<String, Error> {
+    let mut s = String::new();
+    File::open("file_handling_10/src/username.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
 
 fn main() {
-    // let f = File::open("main.jpeg").unwrap();
-    let f = File::open("main.jpeg").expect("‼️ Failed to open main.jpeg");
+    let a = read_username_from_file();
+    println!("{:?}", a);
 }
