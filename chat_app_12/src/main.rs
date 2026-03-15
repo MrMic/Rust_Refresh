56a016a9a5220aea2f2d34a5bbc74e5764f37a19
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-03-15 11:57:44 +0100

    feat(chat_app_12): implement a basic multi-client TCP chat server
    - Create a new package 'chat_app_12' in the workspace.
    - Use 'TcpListener' with non-blocking mode to handle multiple client connections.
    - Implement message broadcasting using 'mpsc::channel' to share messages between client threads and the main loop.
    - Use 'thread::spawn' for each client connection to read and process incoming messages.
    - Add a sleep mechanism to the main loop and client threads to manage resource usage.

---
+++ chat_app_12/src/main.rs
@@ -0,0 +1,68 @@
+#![allow(unused_variables)]
+#![allow(unused_assignments)]
+
+use std::{
+    io::{ErrorKind, Read, Write},
+    net::TcpListener,
+    sync::mpsc,
+    thread::{self},
+    time::Duration,
+};
+
+const LOCAL: &str = "127.0.0.1:6000";
+const MSG_SIZE: usize = 32;
+
+fn main() {
+    let server = TcpListener::bind(LOCAL).expect("Listener failed to bind");
+    server
+        .set_nonblocking(true)
+        .expect("Failed to initialize non-blocking");
+
+    let mut clients = vec![];
+    let (tx, rx) = mpsc::channel::<String>();
+
+    loop {
+        if let Ok((mut socket, addr)) = server.accept() {
+            println!("Client {} connected", addr);
+            let tx = tx.clone();
+            clients.push(socket.try_clone().expect("Failed to clone client"));
+
+            thread::spawn(move || {
+                loop {
+                    let mut buff = vec![0; MSG_SIZE];
+                    match socket.read_exact(&mut buff) {
+                        Ok(_) => {
+                            let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                            let msg =
+                                String::from_utf8(msg).expect("Failed to convert message (utf8)");
+                            println!("Received message: {}", msg);
+                            tx.send(msg).expect("Failed to send message to channel");
+                        }
+                        Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+                        Err(_) => {
+                            println!("Closing connection with: {}", addr);
+                            break;
+                        }
+                    }
+                    sleep();
+                }
+            });
+        }
+        if let Ok(msg) = rx.try_recv() {
+            clients = clients
+                .into_iter()
+                .filter_map(|mut client| {
+                    let mut buff = msg.clone().into_bytes();
+                    buff.resize(MSG_SIZE, 0);
+                    client.write_all(&buff).map(|_| client).ok()
+                })
+                .collect::<Vec<_>>();
+        }
+        sleep();
+    }
+}
+
+// ______________________________________________________________________
+fn sleep() {
+    thread::sleep(Duration::from_millis(100));
+}
